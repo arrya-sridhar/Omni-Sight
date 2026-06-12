354d40b9845bf400982444d3b99d19ff803ca41f
arrya-sridhar <arryasridhar.v@gmail.com>
2026-06-12 22:27:35 +0530

    Clean up test scripts

--- frontend/test_text.js
+++
@@ -1,9 +0,0 @@
-import { pipeline } from '@xenova/transformers';
-
-async function test() {
-  const extractor = await pipeline('feature-extraction', 'Xenova/clip-vit-base-patch32');
-  const textOutput = await extractor("a photo of a cat");
-  console.log(textOutput.dims);
-}
-
-test().catch(console.error);
